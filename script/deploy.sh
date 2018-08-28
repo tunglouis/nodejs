49aec63156acb618a3c4a1b62e85723fa9b0fe49
Tung Pham <louis@Tungs-MBP.net.fpt>
2018-08-28 21:39:14 +0700

    add deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-ssh ezderman@NODE.SERVER.IP <<EOF
- cd ~/node-app
+ssh ubuntu@34.208.215.255 <<EOF
+ cd /home/ubuntu/nodejs
  git pull
+ npm install pm2 -g
  npm install — production
+ pm2 start app.js
  pm2 restart all
  exit
 EOF
